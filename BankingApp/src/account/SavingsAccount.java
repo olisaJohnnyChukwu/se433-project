4cb7a1ed9d0e7ede07965c4f4b6eab56532843ab
Bryan Morandi <bmorandi@depaul.edu>
2021-11-19 19:26:59 -0600

    Laura refactored Account, CheckingAccount, SavingsAccount, and LauraTest

--- BankingApp/src/account/SavingsAccount.java
+++ BankingApp/src/account/SavingsAccount.java
@@ -1,27 +1,64 @@
 package account;
-import java.util.*;
-import transaction.*;
-import customer.*;
-
 
 public class SavingsAccount extends Account{
-    private static final double minimumBalance = 500.00;
-    private static final double interestRate = 0.07;
+    private double minimumBalance;
+    private double interestRate;
     private double interestEarned;
     private double time = 0.0833;
     private double balance;
+    private static int MAXTRANSACTIONNUMBER = 6;
+    private static double ANNUALINTERESTRATE = 0.01;
+    private double principleBalance;
+    private double annualInterestEarned;
+
 
 
     public SavingsAccount(String accountNumber, double balance) {
         super(accountNumber, balance);
         this.balance = balance;
-        
+        setPrincipleBalance(balance);
+
     }
 
+    @Override
+    public double getMinimumBalance() {
+        return minimumBalance;
+    }
 
     @Override
-    public double calculateInterestEarned() {
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setMinimumBalance(double minimumBalance) {
+        this.minimumBalance = minimumBalance;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public void setPrincipleBalance(double principleBalance) {
+        this.principleBalance = principleBalance;
+    }
+
+    public double getPrincipleBalance() {
+        return principleBalance;
+    }
+
+    public int getMaxTransactionNumber() {
+        return MAXTRANSACTIONNUMBER;
+    }
+
+    public double calculateAnnualInterestEarned(){
+        annualInterestEarned=principleBalance*ANNUALINTERESTRATE*1;
+        return annualInterestEarned;
+    }
+    @Override
+    public double calculateInterestEarned(){
         interestEarned = balance * interestRate * time;
         return interestEarned;
     }
+
+
 }
